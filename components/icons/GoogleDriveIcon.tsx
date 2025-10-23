f1fc37bdbc01e0b3ed1238c7a799407530b82e52
alsenan-alt <alsenanwahingcar@gmail.com>
2025-10-23 12:47:20 +0300

    feat(icons): Update Google Drive icon SVG
    Replaces the existing Google Drive icon SVG with a more modern and compact version. This improves the visual consistency and potentially reduces file size.

--- components/icons/GoogleDriveIcon.tsx
+++ components/icons/GoogleDriveIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const GoogleDriveIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props} fill="currentColor">
-        <path d="M416 336H272V480c0 17.67-14.33 32-32 32h-16c-17.67 0-32-14.33-32-32V336H32c-17.67 0-32-14.33-32-32v-16c0-17.67 14.33-32 32-32h160V16c0-8.84 7.16-16 16-16h32c8.84 0 16 7.16 16 16v240h160c17.67 0 32 14.33 32 32v16c0 17.67-14.33 32-32 32z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props} fill="currentColor">
+        <path d="M13.5 2.15L7.43 12.42L1.36 2.15H13.5M14.57 2.15L20.64 12.42L14.57 22.69L8.5 12.42L14.57 2.15M15.64 22.69H22.69L16.62 12.42L15.64 14.15V22.69Z" />
     </svg>
 );
